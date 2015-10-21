55b01165e1705cc2024ae29db95bf95feb425022
Vegard Løkken <vegard@headspin.no>
2015-10-21 09:35:50 +0200

    Read log file from the end for optimization
    Rewritten to read log backwards to make it better suited for large files
    
    Bugfix: Was using == for string comparison instead of eq

--- query_history.pl
+++ query_history.pl
@@ -2,7 +2,7 @@ use strict;
 use vars qw($VERSION %IRSSI);
 
 use Irssi;
-$VERSION = '0.0.3';
+$VERSION = '0.1.0';
 %IRSSI = (
     authors     => 'Vegard Løkken',
     contact     => 'vegard@loekken.org',
@@ -11,8 +11,56 @@ $VERSION = '0.0.3';
     license     => 'MIT'
 );
 
+# Helper function to read a number of lines at the end of file
+# With help from http://stackoverflow.com/a/12017327
+sub log_tail {
+    my ($file, $num_lines) = @_;
+
+    my @result = ();
+    my $count = 0;
+    my $filesize = -s $file;
+    my $offset = -2; # skip two last characters: \n and ^Z in the end of file
+
+    if (open (my $fh, '<:raw', $file)) {
+
+        # Read lines
+        while (abs($offset) <= $filesize && $count < $num_lines) {
+            my $line = '';
+
+            # Read backwards until we reach newline
+            while (abs($offset) <= $filesize) {
+
+                # Move backwards byte by byte
+                seek($fh, $offset--, 2);
+
+                # Get the char
+                my $char = getc($fh);
+
+                # If char is newline, break loop
+                last if ($char eq "\n");
+
+                $line = $char.$line;
+            }
+
+            # Skip "log opened/closed" messages
+            next if ($line =~ /^--- Log (opened|closed)/);
+
+            # Skip "-!- " messages since they're usually status msgs
+            next if ($line =~ /^\d\d:\d\d -!- /);
+
+            # Insert line to beginning of result array
+            unshift(@result, '%b-%n!%b-%n '.$line);
+
+            # Increment counter to know how many lines we've got
+            $count++;
+        }
+    }
+
+    return(@result);
+}
+
 sub sig_query_created {
-    my ($query, $auto) = @_;
+    my $query = @_[0];
     my $qwin = $query->window();
     my $name = $query->{name};
     my $servertag = $query->{server_tag};
@@ -21,17 +69,17 @@ sub sig_query_created {
     #
     # Loop through open logs and find the one that matches
     # name and servertag to get the correct filename.
-    my $filename = '';
+    my $filename;
     my @active_logs = Irssi::logs();
 
     foreach my $log (@active_logs) {
         my $fname = $log->{real_fname};
 
-        foreach my $intermediate ($log->{items}) {
-            foreach my $logitem (@$intermediate) {
+        foreach my $items ($log->{items}) {
+            foreach my $logitem (@$items) {
 
-                if ($logitem->{name} == $name &&
-                    $logitem->{servertag} == $servertag) {
+                if ($logitem->{name} eq $name &&
+                    $logitem->{servertag} eq $servertag) {
 
                     $filename = $fname;
                 }
@@ -39,39 +87,16 @@ sub sig_query_created {
         }
     }
 
-    # Read all log entries
-    #
-    # TODO: Only read end of file for better performance
-    # Hard to know how many lines to read though since we are
-    # stripping unecessary "empty" lines
-    my @log = ();
+    if (defined($filename)) {
 
-    if (open (my $fh, '<:encoding(UTF-8)', $filename)) {
-        ENTRY:
-        while (my $entry = <$fh>) {
-            chomp $entry;
-
-            # Skip "log opened/closed" messages
-            next ENTRY if ($entry =~ /^--- Log (opened|closed)/);
-
-            # Skip "-!- " messages since they're usually status msgs
-            next ENTRY if ($entry =~ /^\d\d:\d\d -!- /);
-
-            push(@log, '%b-%n!%b-%n '.$entry);
-        }
-
-        # Print a number of log entries from the end
-        my $logsize = @log;
+        # Read log entries from the end of log file
         my $max_entries = Irssi::settings_get_int('query_history_num');
-        my @output = ();
+        my @logs = log_tail($filename, $max_entries);
 
-        for (my $i = $logsize-$max_entries; $i < $logsize; $i++) {
-            if ($i > 0) {
-                push(@output, @log[$i]);
-            }
+        # Print to query window
+        if (scalar(@logs) > 0) {
+            $qwin->print(join("\n", @logs), MSGLEVEL_CLIENTCRAP);
         }
-
-        $qwin->print(join("\n", @output), MSGLEVEL_CLIENTCRAP);
 
     } else {
         Irssi::print('query_history.pl: No active log for '.
